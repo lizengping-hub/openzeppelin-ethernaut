01e1d53beff6a112822d7fff0607dd759cff7168
lizengping <lizengping93@gmail.com>
2026-05-15 19:00:30 +0800

    MagicNumSolver bug

--- contracts/src/attacks/MagicNumSolver.sol
+++ contracts/src/attacks/MagicNumSolver.sol
@@ -13,7 +13,7 @@ contract MagicNumSolver {
         // 10 RETURN code: f3
         // Bytecode: 0x602a60005260206000f3 (length 0x0a or 10)
         // Bytecode within a 32 byte word:
-        // 0x00000000000000000000000000000000000000000000604260005260206000f3 (length 0x20 or 32)
+        // 0x00000000000000000000000000000000000000000000602a60005260206000f3 (length 0x20 or 32)
         //                                               ^ (offset 0x16 or 22)
 
 
